8ea3e6b8160b01de67032bb955a5820b8ec48ed7
rzge <trukhanov.vladislav@gmail.com>
2022-07-07 23:59:21 +0300

    добавил fake-useragent

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from bs4 import BeautifulSoup
 import json
 import random
 from time import sleep
+from fake_useragent import UserAgent
 # url = "https://akva-mir.ru/catalog/kulery_dlya_vody/"
 
+ua = UserAgent()
 headers = {
     "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
+    "User-Agent": ua.chrome
 }
 
 # req = requests.get(url, headers=headers)
