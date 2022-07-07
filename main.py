6af5c4b02b23714de202ee2361cdae0c4aaa3743
rzge <trukhanov.vladislav@gmail.com>
2022-07-07 13:53:38 +0300

    pep-8

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import csv
 from bs4 import BeautifulSoup
 import json
 
-#url = "https://akva-mir.ru/catalog/kulery_dlya_vody/"
+# url = "https://akva-mir.ru/catalog/kulery_dlya_vody/"
 
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
 }
 
-#req = requests.get(url, headers=headers)
-#src = req.text
+# req = requests.get(url, headers=headers)
+# src = req.text
 
-#with open("index.html", "w", encoding='utf-8') as file:
+# with open("index.html", "w", encoding='utf-8') as file:
 #    file.write(src)
 
-#with open("index.html", encoding='utf-8') as file:
+# with open("index.html", encoding='utf-8') as file:
 #    src = file.read()
 
-#soup = BeautifulSoup(src, 'lxml')
+# soup = BeautifulSoup(src, 'lxml')
 
 # all_coolers_href = soup.find_all(class_='catalog-top-sections__link')
 # all_coolers_type_dict = {}
@@ -29,10 +29,10 @@ headers = {
 #         cooler_href = 'https://akva-mir.ru' + cooler_type.get('href')  # ссылка на тип кулера для воды
 #         all_coolers_type_dict[cooler_text] = cooler_href
 
-#with open('all_coolers_type_dict.json', 'w', encoding='utf-8') as file: # записыввем типы кулеров в json файл
+# with open('all_coolers_type_dict.json', 'w', encoding='utf-8') as file: # записыввем типы кулеров в json файл
 #    json.dump(all_coolers_type_dict, file, indent=4, ensure_ascii=False)
 
-with open('all_coolers_type_dict.json', encoding='utf-8') as file: #ссылки на типы кулеров по 500 страниц
+with open('all_coolers_type_dict.json', encoding='utf-8') as file:  # ссылки на типы кулеров по 500 страниц
     all_coolers = json.load(file)
 
 # for cooler_type, cooler_href in all_coolers.items(): #берёт ссылки и типы кулера из json файла
@@ -52,4 +52,10 @@ with open('all_coolers_type_dict.json', encoding='utf-8') as file: #ссылки
 #     with open(f'all_coolers_{cooler_type}_dict.json', 'w', encoding='utf-8') as file:  # записыввем типы кулеров в json файл
 #         json.dump(all_coolers_type_species_dict, file, indent=4, ensure_ascii=False)
 
-
+for cooler_type, cooler_href in all_coolers.items():  # берёт ссылки и типы кулера из json файла
+    req = requests.get(url=cooler_href, headers=headers)
+    src = req.text
+    with open(f'data/{cooler_type}.html', 'w', encoding='utf-8') as file:
+        file.write(src)
+    with open(f'data/{cooler_type}.html', encoding='utf-8') as file:
+        src = file.read()
